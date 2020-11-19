d8c9d35305052c70556d11a6c534ce30ecdaa933
zpr1g <yuns.xie@qq.com>
2020-11-19 23:06:29 +0800

    feat: update title

--- site/theme/template/Layout/index.jsx
+++ site/theme/template/Layout/index.jsx
@@ -218,12 +218,8 @@ export default class Layout extends React.Component {
     const { appLocale, direction, isMobile, theme, setTheme, setIframeTheme } = this.state;
     const title =
       appLocale.locale === 'zh-CN'
-        ? 'Ant Design - 一套企业级 UI 设计语言和 React 组件库'
-        : "Ant Design - The world's second most popular React UI framework";
-    const description =
-      appLocale.locale === 'zh-CN'
-        ? '基于 Ant Design 设计体系的 React UI 组件库，用于研发企业级中后台产品。'
-        : 'An enterprise-class UI design language and React UI library with a set of high-quality React components, one of best React UI library for enterprises';
+        ? 'Fluent Design - 一套基于 antd 的 Fluent UI 风格的 React 组件库'
+        : 'Fluent Design - React Fluent UI library, based on antd';
     return (
       <SiteContext.Provider value={{ isMobile, direction, theme, setTheme, setIframeTheme }}>
         <HelmetProvider context={helmetContext}>
@@ -241,7 +237,6 @@ export default class Layout extends React.Component {
               sizes="144x144"
               href="https://gw.alipayobjects.com/zos/antfincdn/UmVnt3t4T0/antd.png"
             />
-            <meta name="description" content={description} />
             <meta property="og:title" content={title} />
             <meta property="og:type" content="website" />
             <meta
