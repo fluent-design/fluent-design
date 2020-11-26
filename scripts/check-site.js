30d0dd08d677a8455969e49cd3a2923457e5a3f0
zpr1g <yuns.xie@qq.com>
2020-11-26 21:15:06 +0800

    Revert "chore: comment test of components temporarily"
    This reverts commit 63cb8f45d8667a43713a7feed8d8c504dc1ff187.

--- scripts/check-site.js
+++ scripts/check-site.js
@@ -3,19 +3,19 @@
 const fetch = require('node-fetch');
 const { join } = require('path');
 const cheerio = require('cheerio');
-// const glob = require('glob');
-// const uniq = require('lodash/uniq');
+const glob = require('glob');
+const uniq = require('lodash/uniq');
 const { createServer } = require('http-server');
 
-// const components = uniq(
-//   glob
-//     .sync('components/*/*.md', {
-//       ignore: '**/{__tests__,_util,version,index.tsx}',
-//       cwd: join(process.cwd()),
-//       dot: false,
-//     })
-//     .map(path => path.replace(/(\/index)?((\.zh-cn)|(\.en-us))?\.md$/i, '')),
-// );
+const components = uniq(
+  glob
+    .sync('components/*/*.md', {
+      ignore: '**/{__tests__,_util,version,index.tsx}',
+      cwd: join(process.cwd()),
+      dot: false,
+    })
+    .map(path => path.replace(/(\/index)?((\.zh-cn)|(\.en-us))?\.md$/i, '')),
+);
 
 describe('site test', () => {
   let server;
@@ -33,16 +33,16 @@ describe('site test', () => {
     return resp;
   };
 
-  // const handleComponentName = name => {
-  //   const componentName = name.split('/')[1];
-  //   return componentName.toLowerCase().replace('-', '');
-  // };
+  const handleComponentName = name => {
+    const componentName = name.split('/')[1];
+    return componentName.toLowerCase().replace('-', '');
+  };
 
-  // const expectComponent = async component => {
-  //   const { status, $ } = await render(`/${component}/`);
-  //   expect(status).toBe(200);
-  //   expect($('.markdown > h1').text().toLowerCase()).toMatch(handleComponentName(component));
-  // };
+  const expectComponent = async component => {
+    const { status, $ } = await render(`/${component}/`);
+    expect(status).toBe(200);
+    expect($('.markdown > h1').text().toLowerCase()).toMatch(handleComponentName(component));
+  };
 
   beforeAll(() => {
     server = createServer({
@@ -59,11 +59,11 @@ describe('site test', () => {
     }
   });
 
-  // it('Basic Pages en', async () => {
-  //   const { status, $ } = await render('/');
-  //   expect($('title').text()).toEqual(`Fluent Design - React Fluent UI library, based on antd`);
-  //   expect(status).toBe(200);
-  // });
+  it('Basic Pages en', async () => {
+    const { status, $ } = await render('/');
+    expect($('title').text()).toEqual(`Fluent Design - React Fluent UI library, based on antd`);
+    expect(status).toBe(200);
+  });
 
   it('Basic Pages zh', async () => {
     const { status, $ } = await render('/index-cn');
@@ -73,16 +73,15 @@ describe('site test', () => {
     expect(status).toBe(200);
   });
 
-  // TODO: components test
-  // for (const component of components) {
-  //   if (component.split('/').length < 3) {
-  //     it(`Component ${component} zh Page`, async () => {
-  //       await expectComponent(component);
-  //     });
+  for (const component of components) {
+    if (component.split('/').length < 3) {
+      it(`Component ${component} zh Page`, async () => {
+        await expectComponent(component);
+      });
 
-  //     it(`Component ${component} en Page`, async () => {
-  //       await expectComponent(component);
-  //     });
-  //   }
-  // }
+      it(`Component ${component} en Page`, async () => {
+        await expectComponent(component);
+      });
+    }
+  }
 });
